58fd5b8bfd7f8b5a18fa52e56cb8642199f4e876
Pekskumar <prakash.montu_92@live.com>
2024-11-16 16:07:44 +0530

    depe. done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "react-confirm-alert/src/react-confirm-alert.css";
+import "react-confirm-alert/src/react-confirm-alert.css";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
